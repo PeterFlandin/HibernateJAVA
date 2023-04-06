3be38df73f6dc0d114405cf832780c3e59693988
PeterFlandin <peterflandin@gmail.com>
2023-04-06 17:40:56 +0200

    supression de update hibernate

--- src/main/java/com/mycompany/tennis/basededonnee/repository/JoueurRepository.java
+++ src/main/java/com/mycompany/tennis/basededonnee/repository/JoueurRepository.java
@@ -21,46 +21,7 @@ public class JoueurRepository {
             System.out.println("Joueur crée");
     }
 
-    public void update(Joueur joueur) {
-        Connection conn = null;
-        try {
-
-            DataSource dataSource = DataSourceProvider.getSingleDataSourceInstance();
-
-
-            //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","root","246810");
-            conn = dataSource.getConnection();
-
-            PreparedStatement preparedStatement = conn.prepareStatement("UPDATE JOUEUR SET NOM=?, PRENOM=? ,SEXE=? WHERE id=?");
-            preparedStatement.setString(1, joueur.getNom());
-            preparedStatement.setString(2, joueur.getPrenom());
-                preparedStatement.setString(3, joueur.getSexe().toString());
-            preparedStatement.setLong(4, joueur.getId());
-
-            preparedStatement.executeUpdate();
-preparedStatement.getGeneratedKeys();
-ResultSet res = preparedStatement.getGeneratedKeys();
-
-            System.out.println("Joueur modifié");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            try {
-                conn.rollback();
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        finally {
-            try {
-                if (conn!=null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
+   
 
 
     public void delete(Long id) {
