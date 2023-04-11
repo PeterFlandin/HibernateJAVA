1f7758cf73cb7982b877e79d23588bd01941991f
PeterFlandin <peterflandin@gmail.com>
2023-04-12 01:18:52 +0200

    score dto avec tournoi & joueur

--- src/main/java/com/mycompany/tennis/basededonnee/dto/ScoreFullDto.java
+++ src/main/java/com/mycompany/tennis/basededonnee/dto/ScoreFullDto.java
@@ -1,39 +1,26 @@
-package com.mycompany.tennis.basededonnee.entity;
+package com.mycompany.tennis.basededonnee.dto;
 
-import javax.persistence.*;
+import com.mycompany.tennis.basededonnee.entity.Match;
 
-@Entity
-@Table(name = "SCORE_VAINQUEUR")
-public class Score {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class ScoreFullDto {
+
     private Long id;
-@Column(name = "SET_1")
     private Byte set1;
-    @Column(name = "SET_2")
 
     private Byte set2;
-    @Column(name = "SET_3")
 
     private Byte set3;
-    @Column(name = "SET_4")
 
     private Byte set4;
-    @Column(name = "SET_5")
 
     private Byte set5;
 
-@OneToOne(fetch = FetchType.LAZY)
-@JoinColumn(name = "id_match")
-    private Match match;
-
-    public Match getMatch() {
-        return match;
-    }
-
-    public void setMatch(Match match) {
-        this.match = match;
-    }
+    private MatchDto matchDto;
 
     public Long getId() {
         return id;
@@ -81,5 +68,13 @@ public class Score {
 
     public void setSet5(Byte set5) {
         this.set5 = set5;
+    }
+
+    public MatchDto getMatchDto() {
+        return matchDto;
+    }
+
+    public void setMatchDto(MatchDto matchDto) {
+        this.matchDto = matchDto;
     }
 }
--- src/main/java/com/mycompany/tennis/basededonnee/service/ScoreService.java
+++ src/main/java/com/mycompany/tennis/basededonnee/service/ScoreService.java
@@ -1,6 +1,10 @@
 package com.mycompany.tennis.basededonnee.service;
 
 import com.mycompany.tennis.basededonnee.HibernateUtil;
+import com.mycompany.tennis.basededonnee.dto.EpreuveFullDto;
+import com.mycompany.tennis.basededonnee.dto.MatchDto;
+import com.mycompany.tennis.basededonnee.dto.ScoreFullDto;
+import com.mycompany.tennis.basededonnee.dto.TournoiDto;
 import com.mycompany.tennis.basededonnee.entity.Joueur;
 import com.mycompany.tennis.basededonnee.entity.Score;
 import com.mycompany.tennis.basededonnee.repository.ScoreRepository;
@@ -16,14 +20,44 @@ public class ScoreService {
         this.scoreRepository = new  ScoreRepository();
     }
 
-    public Score getScore(Long id){
+    public ScoreFullDto getScore(Long id){
         Session session=null;
         Transaction tx = null;
         Score score = null;
+        ScoreFullDto scoreFullDto=null;
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             tx = session.beginTransaction();
             score =  scoreRepository.getById(id);
+
+            scoreFullDto = new ScoreFullDto();
+            scoreFullDto.setId(score.getId());
+            scoreFullDto.setSet1(scoreFullDto.getSet1());
+            scoreFullDto.setSet2(scoreFullDto.getSet2());
+            scoreFullDto.setSet3(scoreFullDto.getSet3());
+            scoreFullDto.setSet4(scoreFullDto.getSet4());
+            scoreFullDto.setSet5(scoreFullDto.getSet5());
+
+            MatchDto matchDto = new MatchDto();
+            matchDto.setId(score.getMatch().getId());
+
+            scoreFullDto.setMatchDto(matchDto);
+
+
+            EpreuveFullDto epreuveDto = new EpreuveFullDto();
+            epreuveDto.setId(score.getMatch().getEpreuve().getId());
+            epreuveDto.setAnnee(score.getMatch().getEpreuve().getAnnee());
+            epreuveDto.setTypeEpreuve(score.getMatch().getEpreuve().getTypeEpreuve());
+            TournoiDto tournoiDto = new TournoiDto();
+            tournoiDto.setId(score.getMatch().getEpreuve().getTournoi().getId());
+            tournoiDto.setNom(score.getMatch().getEpreuve().getTournoi().getNom());
+            epreuveDto.setTournoi(tournoiDto);
+
+            matchDto.setEpreuveFullDto(epreuveDto);
+
+
+
+
             tx.commit();
         } catch (Throwable e) {
             if(tx == null){
@@ -36,7 +70,7 @@ public class ScoreService {
                 session.close();
             }
         }
-        return score;
+        return scoreFullDto;
     }
 
 }
